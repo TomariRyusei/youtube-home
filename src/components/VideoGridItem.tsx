bf57efec19ea2ed3e85c18646524da2241bc5458
TomariRyusei <t.ryu6635@gmail.com>
2023-10-28 22:58:30 +0900

    Done implement video item component

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState } from "react";
+import { formatDuration } from "../utils/formatDuration";
+import { formatTimeAgo } from "../utils/formatTimeAgo";
 
 type VideoGridItemProps = {
   id: string;
@@ -15,6 +17,10 @@ type VideoGridItemProps = {
   videoUrl: string;
 };
 
+const VIEW_FORMATTER = new Intl.NumberFormat("ja-JP", {
+  notation: "compact",
+});
+
 export default function VideoGridItem({
   id,
   title,
@@ -28,18 +34,64 @@ export default function VideoGridItem({
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
 
+  useEffect(() => {
+    if (videoRef.current == null) return;
+
+    if (isVideoPlaying) {
+      videoRef.current.currentTime = 0;
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+  }, [isVideoPlaying]);
+
   return (
-    <div className="flex flex-col gap-2">
+    <div
+      className="flex flex-col gap-2"
+      onMouseEnter={() => setIsVideoPlaying(true)}
+      onMouseLeave={() => setIsVideoPlaying(false)}
+    >
       <a href={`/watch?v=${id}`} className="relative aspect-video">
         <img
           src={thumbnailUrl}
-          className="block w-full h-full object-cover rounded-xl"
+          className={`block w-full h-full object-cover trasition-[border-radius] duration-200 ${
+            isVideoPlaying ? "rounded-none" : "rounded-xl"
+          }}`}
           alt=""
         />
         <div className="absolute bottom-1 right-1 bg-secondary-dark text-secondary text-sm px-0.5 rounded">
-          {duration}
+          {formatDuration(duration)}
         </div>
+        <video
+          ref={videoRef}
+          src={videoUrl}
+          muted
+          playsInline
+          className={`block h-full object-cover absolute inset-0 transition-opacity duration-200 ${
+            isVideoPlaying ? "opacity-100 delay-200" : "opacity-0"
+          }`}
+        ></video>
       </a>
+      <div className="flex gap-2">
+        <a href={`/@${channel.id}`} className="flex-shrink-0">
+          <img
+            src={channel.profileUrl}
+            alt=""
+            className="w-12 h-12 rounded-full"
+          />
+        </a>
+        <div className="flex flex-col">
+          <a href={`/watch?v=${id}`} className="font-bold">
+            {title}
+          </a>
+          <a href={`/@${channel.id}`} className="text-secondary-text text-sm">
+            {channel.name}
+          </a>
+          <div className="text-secondary-text text-sm">
+            {VIEW_FORMATTER.format(views)} Views • {formatTimeAgo(postedAt)}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
